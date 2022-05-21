0793050525347f67f3b3d63218a49891d8ee3046
Edmund <edmundlzen@gmail.com>
2022-05-21 15:00:26 +0800

    Added cool map generation system

--- App.tsx
+++ App.tsx
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import ReactNativeZoomableView from '@openspacelabs/react-native-zoomable-view/src/ReactNativeZoomableView';
+
+const gridCount = 2500;
+const gridSize = 50;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.mapContainer}>
+				<ReactNativeZoomableView
+					maxZoom={3}
+					minZoom={0.1}
+					zoomStep={0.5}
+					initialZoom={0.1}
+					bindToBorders={false}
+					movementSensibility={5}
+					style={{
+						backgroundColor: 'red',
+					}}
+				>
+					<View style={styles.map}>
+						{[...Array(gridCount)].map((x, i) =>
+							<View key={i} style={styles.grid}>
+								<Text>{i}</Text>
+							</View>
+						)}
+					</View>
+				</ReactNativeZoomableView>
+			</View>
+			<StatusBar style="auto"/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	mapContainer: {
+		width: '80%',
+		height: '80%',
+		borderWidth: 1,
+		borderColor: 'black',
+	},
+	map: {
+		width: Math.ceil(Math.sqrt(gridCount)) * gridSize,
+		height: Math.ceil(Math.sqrt(gridCount)) * gridSize,
+		backgroundColor: 'green',
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		overflow: 'hidden'
+	},
+	grid: {
+		width: 50,
+		height: 50,
+		backgroundColor: '#cccccc',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
